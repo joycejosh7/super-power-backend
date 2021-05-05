6bad162be049d75aa6073547c8226758247f9751
Joshua <joycejosh7@gmail.com>
2021-05-05 15:14:26 -0400

    added destroy method

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -3,5 +3,10 @@ class PowersController < ApplicationController
     def index
         render json: Power.all
     end
-    
+
+    def destroy
+        power = Power.find_by(id: params[:id])
+        power.destroy
+        render json: {message: "Power Lost!!"}
+    end
 end
